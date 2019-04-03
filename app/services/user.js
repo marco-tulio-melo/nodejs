ab710be7196971233ea91c7580f39bbc4bddc595
Stanley Gomes <stanleygomesdasilva@gmail.com>
2019-04-02 21:57:10 -0300

    a bit of javascript

--- app/services/user.js
+++ app/services/user.js
@@ -4,7 +4,31 @@ const router = express.Router()
 const userBO = require('../bo/userBO')
 
 router.get('', (req, res) => {
-  console.log(userBO.test)
+
+  let a = 0,
+    b = 0,
+    d = 'teste'
+
+  let c = a / b
+
+  console.log('a eh: ' + typeof a)
+  console.log('d eh: ' + typeof d)
+
+  console.log("Amarildo" < "Zenaide")
+  console.log("Zenaide" < "america")
+
+  console.log('null == undefined => ' + (null == undefined));
+  console.log('null == 0 => ' + (null == 0));
+
+  // coalesce like in javascript
+  console.log(null || "user")
+  console.log("Agnes" || "user")
+
+  console.log(`a / b = ${c}`)
+  console.log(Number.isNaN(c))
+
+  // console.log(userBO.test)
+  res.send('hello world 2')
 }).get('/teste2', (req, res) => {
   console.log('teste 2')
   res.send('hello world 2')
