1cb83f89f370d2de6adb10c0222c2a67cedce7ae
Stanley Gomes <stanleygomesdasilva@gmail.com>
2019-04-16 09:13:43 -0300

    teste imc e operadores

--- src/eloquent-javascript/operators/operators.js
+++ src/eloquent-javascript/operators/operators.js
@@ -2,4 +2,12 @@ const operators = {}
 
 operators.helloWorld = () => 'hello world!'
 
+operators.testTypeOf = (v) => {
+  return typeof v
+}
+
+operators.countList = (list) => {
+  return list.length
+}
+
 module.exports = operators
--- src/test/operators-test.js
+++ src/test/operators-test.js
@@ -8,4 +8,20 @@ describe('Testes para operadores', () => {
   it('Validar resposta do hello world', () => {
     assert.equal(operators.helloWorld(), 'hello world!')
   })
+
+  it('Validar o tipo de variavel de entrada (string)', () => {
+    expect(operators.testTypeOf('teste')).to.equal('string')
+  })
+
+  it('Validar o tipo de variavel de entrada (number)', () => {
+    expect(operators.testTypeOf(3.5)).to.equal('number')
+  })
+
+  it('Validar o tipo de variavel de entrada (object)', () => {
+    expect(operators.testTypeOf({a: 0})).to.equal('object')
+  })
+
+  it('Validar o numero de itens de uma lista', () => {
+    expect(operators.countList([1,2,3])).to.equal(3)
+  })
 })
